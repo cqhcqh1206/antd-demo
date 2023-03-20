d47efa3d554ee279104c342426fd2f3d824e9187
cqhcqh1206 <cqhcqh1206@163.com>
2023-03-20 12:45:43 +0800

    ci 页面

--- src/App.js
+++ src/App.js
@@ -38,22 +38,6 @@ const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
   },
 );
 
-const contentStyle = {
-  margin: 0,
-  height: '160px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
-};
-
-const { Panel } = Collapse;
-
-const text = `
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.
-`;
 
 
 
@@ -105,36 +89,7 @@ const App = () => {
               background: colorBgContainer,
             }}
           >
-
-
-             <Carousel afterChange={onChange}>
-                <div>
-                  <h3 style={contentStyle}>1</h3>
-                 </div>
-                 <div>
-                    <h3 style={contentStyle}>2</h3>
-                  </div>
-                 <div>
-                    <h3 style={contentStyle}>3</h3>
-                  </div>
-                 <div>
-                   <h3 style={contentStyle}>4</h3>
-                 </div>
-             </Carousel>
-
-             <p>{ }</p>
-
-             <Collapse defaultActiveKey={['1']} onChange={onChangetwo}>
-               <Panel header="This is panel header 1" key="1">
-                  <p>{text}</p>
-                </Panel>
-               <Panel header="This is panel header 2" key="2">
-                   <p>{text}</p>
-                </Panel>
-               <Panel header="This is panel header 3" key="3">
-                   <p>{text}</p>
-               </Panel>
-              </Collapse>
+             
               
               {/* <Routes>
                  <Route path="/class1" Component={Class1} />
