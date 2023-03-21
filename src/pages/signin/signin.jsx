16d3740c91215120ffed17a65d9d3984bff4c3d6
cqhcqh1206 <cqhcqh1206@163.com>
2023-03-21 15:31:54 +0800

    ci signin

--- src/pages/signin/signin.jsx
+++ src/pages/signin/signin.jsx
@@ -1,38 +1,77 @@
-import React from 'react';
-import { UserOutlined,EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
-import { InpButton, Input, Spaceut } from 'antd';
-
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Form, Input, Space } from "antd";
+import { useState } from "react";
 const App = () => {
-  <>
-    <Input size="large" placeholder="large size" prefix={<UserOutlined />} />
-    <br />
-    <br />
-    <Input placeholder="default size" prefix={<UserOutlined />} />
-    <br />
-    <br />
-    <Input size="small" placeholder="small size" prefix={<UserOutlined />} />
-  </>
-     const [passwordVisible, setPasswordVisible] = React.useState(false);
+  const [size, setSize] = useState("large");
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+  return (
+    <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Username!",
+          },
+        ]}
+      >
+        <Input
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder="Username"
+        />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Password!",
+          },
+        ]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
 
-     return (
-        <Space direction="vertical">
-          <Input.Password placeholder="input password" />
-          <Input.Password
-            placeholder="input password"
-            iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-          />
-          <Space direction="horizontal">
-            <Input.Password
-              placeholder="input password"
-              visibilityToggle={{ visible: passwordVisible, onVisibleChange: setPasswordVisible }}
-            />
-            <Button style={{ width: 80 }} onClick={() => setPasswordVisible((prevState) => !prevState)}>
-              {passwordVisible ? 'Hide' : 'Show'}
-            </Button>
-          </Space>
+        <a className="login-form-forgot" href="">
+          Forgot password
+        </a>
+      </Form.Item>
+
+      <Form.Item>
+        <Space size={size}>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            登录
+          </Button>
+          <Button /* type="primary" htmlType="submit" className="login-form-button" */
+          >
+            注册
+          </Button>
+          {/* Or <a href="">register now!</a> */}
         </Space>
-      );
-
-     };
+      </Form.Item>
+    </Form>
+  );
+};
 
 export default App;
