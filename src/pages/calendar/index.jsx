14d95c4975fa9a38758097d2f9c90276fa650512
cqhcqh1206 <cqhcqh1206@163.com>
2023-05-08 22:38:04 +0800

    ci calander

--- src/pages/calendar/index.jsx
+++ src/pages/calendar/index.jsx
@@ -1,8 +1,28 @@
 import { Calendar } from 'antd';
+import { Tree } from 'antd';
+
+const dig = (path = '0', level = 3) => {
+  const list = [];
+  for (let i = 0; i < 10; i += 1) {
+    const key = `${path}-${i}`;
+    const treeNode = {
+      title: key,
+      key,
+    };
+    if (level > 0) {
+      treeNode.children = dig(key, level - 1);
+    }
+    list.push(treeNode);
+  }
+  return list;
+};
+const treeData = dig();
+
 const App = () => {
   const onPanelChange = (value, mode) => {
     console.log(value.format('YYYY-MM-DD'), mode);
   };
   return <Calendar onPanelChange={onPanelChange} />;
+  <Tree treeData={treeData} height={233} defaultExpandAll />;
 };
 export default App;
